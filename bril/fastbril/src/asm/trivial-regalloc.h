c55798c4a0f9c99447b477dfd30aac61b996e7cf
cseslowpoke <cseslowpoke@gmail.com>
2024-08-26 11:05:16 +0800

    add bril as submodule

--- bril/fastbril/src/asm/trivial-regalloc.h
+++
@@ -1,7 +0,0 @@
-#ifndef TRIVREG_H
-#define TRIVREG_H
-#include "asm.h"
-
-asm_prog_t triv_allocate(asm_prog_t prog);
-
-#endif
--- bril/fastbril/srcgen.awk
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env -S awk -f
-
-{
-    if($0 != "")
-	print "#define " $1 " " $2;
-}
