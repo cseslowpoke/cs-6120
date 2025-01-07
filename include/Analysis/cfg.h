13fd7fe9bfa512f29e502a5eb3e62a9aad221678
cseslowpoke <cseslowpoke@gmail.com>
2025-01-07 18:30:34 +0800

    Refactor: Improve BlockMap and CFG construction process

--- include/Analysis/cfg.h
+++ include/Analysis/cfg.h
@@ -1,7 +1,8 @@
 /*
  * Control Flow Graph
-*/
+ */
 #pragma once
+#include <map>
 #include <nlohmann/json.hpp>
 #include <set>
 
@@ -10,8 +11,25 @@ static std::set<std::string> terminator = {"jmp", "br", "ret"};
 
 json form_block(json &body);
 
-std::vector<std::pair<std::string, json>> block_map(json &body);
+std::map<std::string, json> block_map(json &body);
 
-std::vector<std::pair<std::string, std::string> > get_cfg(std::vector<std::pair<std::string, json>> &blockmap);
+class BlockMap {
+public:
+  BlockMap(json &body);
+  const json &operator[](std::string index) const {
+    return order[index_map.at(index)];
+  }
+  const json &operator[](size_t index) const { return order[index]; }
+  const size_t size() const { return order.size(); }
+  const std::string &getname(size_t index) const { return name_map.at(index); }
+  const size_t getindex(std::string name) const { return index_map.at(name); }
 
-void cfg();
+private:
+  std::vector<json> order;
+  std::map<std::string, size_t> index_map;
+  std::map<size_t, std::string> name_map;
+};
+
+std::map<std::string, std::vector<std::string>> get_cfg(BlockMap &blockmap);
+
+// void cfg();
