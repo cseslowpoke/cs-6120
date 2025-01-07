3eaca5a10e51b60eb0e729f97a0ae2bad67391b7
cseslowpoke <cseslowpoke@gmail.com>
2025-01-07 18:30:34 +0800

    Refactor: Improve BlockMap and CFG construction process

--- lib/Analysis/cfg.cpp
+++ lib/Analysis/cfg.cpp
@@ -27,57 +27,69 @@ json form_block(json &body) {
   return func;
 }
 
-std::vector<std::pair<std::string, json>> block_map(json &body) {
-  std::vector<std::pair<std::string, json>> name2block;
+BlockMap::BlockMap(json &body) {
   for (auto &block : body) {
     std::string name;
     if (block[0].contains("label")) {
       name = block[0]["label"];
     } else {
-      name = "b" + std::to_string(name2block.size());
+      name = "b" + std::to_string(order.size());
     }
-    name2block.push_back({name, block});
+    index_map.insert({name, order.size()});
+    name_map.insert({order.size(), name});
+    order.push_back(block);
   }
-  return name2block;
 }
 
-std::vector<std::pair<std::string, std::string>>
-get_cfg(std::vector<std::pair<std::string, json>> &blockmap) {
-  std::vector<std::pair<std::string, std::string>> cfg;
-  for (size_t i = 0; i < blockmap.size(); i++) {
-    auto &[name, block] = blockmap[i];
+// std::map<std::string, json> block_map(json &body) {
+//   std::map<std::string, json> name2block;
+//   for (auto &block : body) {
+//     std::string name;
+//     if (block[0].contains("label")) {
+//       name = block[0]["label"];
+//     } else {
+//       name = "b" + std::to_string(name2block.size());
+//     }
+//     name2block.insert({name, block});
+//   }
+//   return name2block;
+// }
+
+std::map<std::string, std::vector<std::string>> get_cfg(BlockMap &blockmap) {
+  std::map<std::string, std::vector<std::string>> cfg;
+  for (int i = 0; i < blockmap.size(); i++) {
+    auto &block = blockmap[i];
+    auto &name = blockmap.getname(i);
     auto &last = block.back();
     if (!last.contains("op")) {
-      if (i != blockmap.size()) {
-        cfg.push_back({name, blockmap[i + 1].first});
+      if (i != blockmap.size() - 1) {
+        cfg[name].push_back(blockmap.getname(i + 1));
       }
     } else if (last.at("op") == "ret") {
-      cfg.push_back({name, " "});
-      continue;
+      cfg[name].push_back(" ");
     } else if (last.at("op") == "jmp") {
-      cfg.push_back({name, last.at("labels")[0]});
-    } else if (last.back() == "br") {
-      cfg.push_back({name, last.at("labels")[0]});
-      cfg.push_back({name, last.at("labels")[1]});
+      cfg[name].push_back(last.at("label")[0]);
+    } else if (last.at("op") == "br") {
+      cfg[name].push_back(last.at("label")[0]);
+      cfg[name].push_back(last.at("label")[1]);
     } else {
-      if (i != blockmap.size() - 1) {
-        cfg.push_back({blockmap[i].first, blockmap[i + 1].first});
-      }
+      cfg[name].push_back(blockmap.getname(i + 1));
     }
   }
   return cfg;
 }
 
-void cfg(json prog) {
-  for (auto func : prog["functions"]) {
-    auto block = form_block(func["instrs"]);
-    auto name2block = block_map(block);
-    for (auto &[name, block] : name2block) {
-      // std::cout << name << ": \n  " << block.dump(-1) << std::endl;
-    }
-    auto cfg = get_cfg(name2block);
-    for (auto &[from, to] : cfg) {
-      // std::cout << from << " -> " << to << std::endl;
-    }
-  }
-}
+// void cfg(json prog) {
+//   for (auto func : prog["functions"]) {
+//     auto form = form_block(func["instrs"]);
+//     auto blockmap = BlockMap(form);
+//     for (size_t i = 0; i < blockmap.size(); i++) {
+//       auto &block = blockmap[i];
+//       // std::cout << name << ": \n  " << block.dump(-1) << std::endl;
+//     }
+//     auto cfg = get_cfg(blockmap);
+//     for (auto &[from, to] : cfg) {
+//       // std::cout << from << " -> " << to << std::endl;
+//     }
+//   }
+// }
